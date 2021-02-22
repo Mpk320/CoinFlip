9b906d714e291f496088773e8cc6cf947e12cdc7
Wes | Atro <wesley.reid@inxabyss.com>
2021-02-22 18:01:27 -0500

    Cleaned up formating

--- Program.cs
+++ Program.cs
@@ -10,14 +10,13 @@ namespace Coin_flip
         Console.WriteLine("Press enter to flip a coin");
         Console.ReadLine();
         string flip = flipCoin();
-
+        Console.WriteLine(flip);
         }
 
     static string flipCoin()
         {
         Random rnd = new Random();
-        if ( rnd.Next() % 2 ==0){
-            //Console.WriteLine("Even");
+        if (rnd.Next() % 2 ==0){
             return "Tails";
             }
         return "Heads";
