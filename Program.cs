aeceeb6a0afe85066bd3dfecbc3163da89c8ed39
Mpk320 <mkasunick@protonmail.com>
2021-02-22 16:35:57 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Coin_flip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Press enter to flip a coin");
+            Console.ReadLine();
+
+            Console.WriteLine();
+            coin()
+
+        }
+
+       static int coin(int num)
+       {
+            bool flip = rand;
+            if(flip == true)
+            Console.WriteLine("Tails");
+
+            if(flip == false)
+            Console.WriteLine("Heads");
+       }
+
+       static int random(int rand)
+       {
+           int[] randNum = new int[] {1, 2};
+           
+       }
+    }
+}
